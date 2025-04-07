8ca4caf3f6d5a74cfac1aadff7085b42200c0361
Camilla Valtameri Shen <camilla@Camillas-MacBook-Pro.local>
2025-04-07 02:28:02 -0700

    session 2

---
+++ journal1/s2.py
@@ -0,0 +1,28 @@
+float1 = 3.6
+float2 = 5.2
+
+def float_sum(f1, f2):
+	result = f1 + f2
+	print(result)
+	print(type(result))	
+
+int1 = 4
+int2 = 2
+
+def int_diff(i1, i2):
+	result = i1 - i2
+	print(result)
+	print(type(result))
+
+a = 2.3
+b = 9
+
+def product(f, i):
+	result = f * i
+	print(result)
+	print(type(result))
+
+float_sum(float1, float2)
+int_diff(int1, int2)
+product(a, b)
+
